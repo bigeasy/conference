ce0d0ecc920fda848d1347e2d000d4b30e483df9
Alan Gutierrez <alan@prettyrobots.com>
2021-09-30 10:25:33 -0500

    Build `README.md` with Edify.
    See #87.

---
+++ test/readme.t.js
@@ -0,0 +1,35 @@
+// [![Actions Status](https://github.com/bigeasy/ascension/workflows/Node%20CI/badge.svg)](https://github.com/bigeasy/ascension/actions)
+// [![codecov](https://codecov.io/gh/bigeasy/ascension/branch/master/graph/badge.svg)](https://codecov.io/gh/bigeasy/ascension)
+// [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+//
+// Reduce messages from an atomic log.
+//
+// | What          | Where                                         |
+// | --- | --- |
+// | Discussion    | https://github.com/bigeasy/conference/issues/1 |
+// | Documentation | https://bigeasy.github.io/conference           |
+// | Source        | https://github.com/bigeasy/conference          |
+// | Issues        | https://github.com/bigeasy/conference/issues   |
+// | CI            | https://travis-ci.org/bigeasy/conference       |
+// | Coverage:     | https://codecov.io/gh/bigeasy/conference       |
+// | License:      | MIT                                           |
+//
+// Ascension installs from NPM.
+
+// ## Living `README.md`
+//
+// This `README.md` is also a unit test using the
+// [Proof](https://github.com/bigeasy/proof) unit test framework. We'll use the
+// Proof `okay` function to assert out statements in the readme. A Proof unit test
+// generally looks like this.
+
+require('proof')(1, okay => {
+    // ## Overview
+
+    {
+        okay('okay')
+    }
+})
+
+// You can run this unit test yourself to see the output from the various
+// code sections of the readme.
